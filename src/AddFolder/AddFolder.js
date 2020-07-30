62853fbe310a2592a7b6dcba963abc9bd6328adb
Your Name <you@example.com>
2020-07-30 13:33:10 -0400

    added post

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react';
+import ApiContext from '../ApiContext'
 
 export default class AddFolder extends Component{
+    static contextType = ApiContext
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.context.folders)
+    }
+    
     render(){
         return(
-            <div class="folder-form">
-                <form className="newFolder">
+            <div className="folder-form" >
+                <form className="newFolder" onSubmit={ e => this.handleSubmit(e) }>
                     <label htmlFor="folderName">Folder Name</label>
                     <input 
                         type="text"
                         className="folderName"
                         id="id"
                         />     
-                </form>
                 <button type="submit" value="submit">Submit</button>
+                </form>
             </div>
         )
     }
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,22 +1,63 @@
 import React, {Component} from 'react';
+import ApiContext from '../ApiContext'
+import config from '../config'
 
 export default class AddNote extends Component{
+    static defaultProps ={   
+        addNote: () => {},
+    }
+
+    state = {
+        name: "",
+        content: "",
+    }
+
+    static contextType = ApiContext
+    
+    handleSubmit = e => {
+        e.preventDefault()
+        const noteId = this.props.id
+        console.log(this.context.notes)
+    const  {name, content} = this.state
+    const note = {name, content}
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+        method: 'POST',
+        body:JSON.stringify(note)
+        // headers: {
+        //   'content-type': 'application/json'
+        // },
+      })
+      .then(res => {
+        if (!res.ok)
+            return res.json().then(e => Promise.reject(e))
+            return res.json()
+        })
+        .then(() => {
+        this.context.addNote(noteId)
+        // allow parent to perform extra behaviour
+        // this.props.onDeleteNote(noteId)
+        })
+        .catch(error => {
+        console.error({ error })
+        })
+    }
+    
     render(){
         return(
-            <div class="note-form">
-                <form className="newNote">
-                    <label htmlFor="noteName">Folder Name</label>
+            <div className="note-form">
+                <form className="newNote" onSubmit={ e => this.handleSubmit(e) }>
+                    <label htmlFor="note-name">Note Name</label>
                     <input 
                         type="text"
-                        className="noteName"
+                        className="note-name"
                         id="id"
                         />
                     <input
                         type="text"
-                        className="noteContent"
+                        className="note-content"
                         />   
-                </form>
                 <button type="submit" value="submit">Submit</button>
+                </form>
             </div>
         )
     }
