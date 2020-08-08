d03eef80e2179cf3e485bbee4b105f57cecb8db2
Jackie Abert <jax.abert@gmail.com>
2020-08-08 00:19:58 -0400

    fixed form validation for dropdown menu for folder selection in note creation. fixed ability to add noted to folders using dropdown menu. works as it should.

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -6,31 +6,36 @@ import './AddNote.css'
 
 export default class AddNote extends Component {
     static contextType = ApiContext;
-
     state = {
-        name:{
-            value:"",
-            touched:false
+        name: {
+            value: "",
+            touched: false
         },
-        content:{
-            value:"",
-            touched:false
+        content: {
+            value: "",
+            touched: false
         },
-        id:"",
+        folder: {
+            value: "",
+            touched: false
+        },
+        id: "",
     }
-
     updateName(name) {
         this.setState({
-            name:{value: name, touched: true}
+            name: { value: name, touched: true }
         })
     }
-
     updateContent(content) {
         this.setState({
-            content:{value: content, touched: true}
+            content: { value: content, touched: true }
         })
     }
-
+    updateFolder = (id) => {
+        this.setState({
+            folder: { value: id, touched: true }
+        });
+    };
     validateName() {
         const name = this.state.name
         if (name.value.length === 0) {
@@ -43,29 +48,32 @@ export default class AddNote extends Component {
             return "Content is required";
         }
     }
+    validateFolder() {
+        const folder = this.state.folder
+        console.log(folder)
+        if (folder.value === "") {
+            return "Selection is required";
+        }
+    }
     getFolders() {
-    return this.context.folders.map((folder) => {
-        return (
-          <option key={folder.id} value={folder.id}>
-            {folder.name}
-          </option>
-        );
-      });
+        return this.context.folders.map((folder) => {
+            return (
+                <option key={folder.id} value={folder.id}>
+                    {folder.name}
+                </option>
+            );
+        });
     };
-    updateFolder = (id) => {
-        this.setState({ folder: { value: id, touched: true } });
-      };
-
     handleSubmit = e => {
         e.preventDefault()
         console.log(this.context.notes)
-        const { name, content, folder} = this.state
-
-        const note = { 
-            name:name.value, 
-            content:content.value,
+        const { name, content, folder } = this.state
+        const note = {
+            name: name.value,
+            content: content.value,
             folderId: folder.value,
-            modified:new Date().toLocaleString()}
+            modified: new Date().toLocaleString()
+        }
         console.log(e)
         fetch(`${config.API_ENDPOINT}/notes/`, {
             method: 'POST',
@@ -77,14 +85,13 @@ export default class AddNote extends Component {
             .then(res => {
                 if (!res.ok)
                     return res.json()
-            .then(e => Promise.reject(e))
+                        .then(e => Promise.reject(e))
                 return res.json()
             })
             .then((data) => {
                 this.context.addNote(data)
                 this.props.history.push("/")
             })
-
             .catch(error => {
                 console.error({ error })
             })
@@ -93,10 +100,10 @@ export default class AddNote extends Component {
         e.preventDefault();
         this.props.history.push("/");
     }
-    
     render() {
         const nameError = this.validateName();
         const contentError = this.validateContent();
+        const selectError = this.validateFolder();
         return (
             <div className="note-form">
                 <h2>Add New Note</h2>
@@ -118,22 +125,20 @@ export default class AddNote extends Component {
                         onChange={e => this.updateContent(e.target.value)}
                     />
                     {this.state.name.touched && <ValidationError message={contentError} />}
-                    <select
-                        name="folder"
-                        onChange={(e) => this.updateFolder(e.target.value)}
-                        >
-                        <option value="">Choose</option>
+                    <select name="select" onChange={(e) => this.updateFolder(e.target.value)}>
+                        <option value="" name="select">Choose</option>
                         {this.getFolders()}
                     </select>
-                    <button 
-                        type="submit" 
+                    {this.state.name.touched && <ValidationError message={selectError} />}
+                    <button
+                        type="submit"
                         value="submit"
                         disabled={
                             this.validateName() ||
-                            this.validateName()
+                            this.validateContent() ||
+                            this.validateFolder()
                         }
-                        >Submit</button><button type="button" onClick={e => this.handleCancel(e)}>Cancel</button>
-                        
+                    >Submit</button><button type="button" onClick={e => this.handleCancel(e)}>Cancel</button>
                 </form>
             </div>
         )
